be26f190f7b15395b7618f54c3525b241c10e5ec
Muhammad Adi Anugerah Arrahman <5025241118@student.its.ac.id>
2025-12-07 16:37:03 +0700

    FrontEnd

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// assets/js/script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
